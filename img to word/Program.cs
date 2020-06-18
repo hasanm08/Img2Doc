4733b434a0b2e6ca946e6b54a7ea91fddb498a02
hasanm08 <hasanmahani08@gmail.com>
2020-06-18 21:22:41 +0430

    Add print feature

--- img to word/Program.cs
+++ img to word/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Office.Interop.Word;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,11 @@ namespace img_to_word
         static void Main(string[] args)
         {
             List<string> vs = new List<string>();
-            for (int i = 41; i > 0; i--)
+            Console.WriteLine("Pleasse Enter num of pics :");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i > 0; i--)
             {
-                vs.Add(@"C:\Users\hasanm08\Downloads\Telegram Desktop\System Digital\" + i.ToString() + ".JPG");
+                vs.Add(@"C:\Users\L340\Desktop\signal\Signal (" + i.ToString() + ").JPG");
             }
             imgtodoc(vs);
         }
@@ -29,15 +32,23 @@ namespace img_to_word
             WordApp.Visible = true;
             // get the reference of active document
             Microsoft.Office.Interop.Word.Document doc = WordApp.ActiveDocument;
+
+            doc.Content.Text += "\n\n\nCreatedBy ImageToDoc C# App \n Please Report any problems";
             foreach (string item in imgs)
             {
+
                 doc.InlineShapes.AddPicture(item, Type.Missing, Type.Missing, Type.Missing);
                 Console.WriteLine(item + " Done!");
             }
             // file is saved.
-            doc.SaveAs(@"C:\Users\hasanm08\Downloads\Telegram Desktop\System Digital\hello.doc", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            doc.SaveAs(@"C:\Users\L340\Desktop\signal\hello.doc", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
             // application is now quit.
             WordApp.Quit(Type.Missing, Type.Missing, Type.Missing);
+            ProcessStartInfo info = new ProcessStartInfo(@"C:\Users\L340\Desktop\signal\hello.doc");
+            info.Verb = "Print";
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(info);
         }
     }
 }
